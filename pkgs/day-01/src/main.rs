d4a991362139181bbdbcbe50fe9da6766dbe75df
nulladdict <nulladdicted@gmail.com>
2021-12-01 11:51:59 +0500

    feat: add day-01

--- pkgs/day-01/src/main.rs
+++ pkgs/day-01/src/main.rs
@@ -1,3 +1,33 @@
+use itertools::Itertools;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let measurements = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|m| {
+            let m = m.unwrap();
+            let m = m.trim();
+            m.parse::<u16>().unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    let part1 = count_increase(&measurements);
+    dbg!(part1);
+
+    let measurements = measurements
+        .iter()
+        .tuple_windows()
+        .map(|(x, y, z)| x + y + z)
+        .collect::<Vec<_>>();
+
+    let part2 = count_increase(&measurements);
+    dbg!(part2);
+}
+
+fn count_increase<T: Ord + Copy>(xs: &[T]) -> usize {
+    xs.iter()
+        .zip(xs.iter().skip(1))
+        .filter(|(x, y)| y > x)
+        .count()
 }
