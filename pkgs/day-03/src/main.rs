c67facad6b17db7324640265af74851a29f39bc3
nulladdict <nulladdicted@gmail.com>
2021-12-03 13:55:40 +0500

    feat: add day-03

---
+++ pkgs/day-03/src/main.rs
@@ -0,0 +1,74 @@
+const BIT_LENGTH: usize = 12;
+const MASK: usize = (1 << BIT_LENGTH) - 1;
+
+fn main() {
+    let numbers = include_str!("in")
+        .lines()
+        .map(|s| {
+            assert_eq!(s.len(), BIT_LENGTH);
+            usize::from_str_radix(s, 2).unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    dbg!(part1(&numbers));
+    dbg!(part2(&numbers));
+}
+
+fn part1(numbers: &[usize]) -> usize {
+    let mut gamma_rate = 0;
+    let size = numbers.len();
+    for offset in 0..BIT_LENGTH {
+        let ones = numbers
+            .iter()
+            .filter(|&&number| bit_at(number, offset) == 1)
+            .count();
+        if ones > size / 2 {
+            gamma_rate += 1 << offset;
+        }
+    }
+    let epsilon_rate = !gamma_rate & MASK;
+    gamma_rate * epsilon_rate
+}
+
+fn bit_at(number: usize, offset: usize) -> u8 {
+    (number >> offset & 1) as u8
+}
+
+fn part2(numbers: &[usize]) -> usize {
+    let oxygen_rating = bit_fuckery(numbers, BIT_LENGTH - 1, most_common);
+    let c02_rating = bit_fuckery(numbers, BIT_LENGTH - 1, least_common);
+    oxygen_rating * c02_rating
+}
+
+fn most_common(ones: usize, zeros: usize) -> u8 {
+    (ones >= zeros) as u8
+}
+
+fn least_common(ones: usize, zeros: usize) -> u8 {
+    (ones < zeros) as u8
+}
+
+fn bit_fuckery<C>(numbers: &[usize], offset: usize, criteria: C) -> usize
+where
+    C: Fn(usize, usize) -> u8,
+{
+    let size = numbers.len();
+    let ones = numbers
+        .iter()
+        .filter(|&&number| bit_at(number, offset) == 1)
+        .count();
+    let zeros = size - ones;
+    let the_bit = criteria(ones, zeros);
+
+    let numbers = numbers
+        .iter()
+        .filter(|&&number| bit_at(number, offset) == the_bit)
+        .cloned()
+        .collect::<Vec<_>>();
+
+    if numbers.len() == 1 {
+        return numbers[0];
+    }
+
+    bit_fuckery(&numbers, offset.checked_sub(1).unwrap(), criteria)
+}
